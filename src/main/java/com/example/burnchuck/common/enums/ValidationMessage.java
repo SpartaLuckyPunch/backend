675f8ef017f56fa26e47f76d920c0e11d9b5bd0c
Thugi-98 <kimjh4533@gmail.com>
2026-01-16 12:04:34 +0900

    chore: ValidationMessage 추가
    - Validation Message에 String을 직접 넣는 대신 ValidationMessage의 String 값을 가져와서 사용

---
+++ src/main/java/com/example/burnchuck/common/enums/ValidationMessage.java
@@ -0,0 +1,10 @@
+package com.example.burnchuck.common.enums;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ValidationMessage {
+
+    // --- User 관련 ---
+    public static final String USERNAME_NOT_BLANK = "닉네임은 필수입니다.";
+}
