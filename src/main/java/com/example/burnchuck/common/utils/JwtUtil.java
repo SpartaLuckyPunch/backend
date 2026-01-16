d456a18580cfa4a52f62f89fe3d512d9fe25b93f
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-01-16 11:12:33 +0900

    feat: JWT Util 설정
    - application.yml : jwt 시크릿키 설정 추가
    - JWT Util : 토큰 생성, 검증, 복호화 작업

---
+++ src/main/java/com/example/burnchuck/common/utils/JwtUtil.java
@@ -0,0 +1,81 @@
+package com.example.burnchuck.common.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    public static final String BEARER_PREFIX = "Bearer ";
+    private static final long TOKEN_TIME = 60 * 60 * 1000L; // 1시간
+
+    @Value("${jwt.secret.key}")
+    private String secretKeyString;
+
+    private SecretKey key;
+    private JwtParser parser;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Decoders.BASE64.decode(secretKeyString);
+        this.key = Keys.hmacShaKeyFor(bytes);
+        this.parser = Jwts.parser()
+            .verifyWith(this.key)
+            .build();
+    }
+
+    // 토큰 생성
+    public String generateToken(Long id, String email, String nickname) {
+        Date now = new Date();
+
+        return BEARER_PREFIX + Jwts.builder()
+            .claim("id", id)
+            .claim("email", email)
+            .claim("nickname", nickname)
+            .issuedAt(now)
+            .expiration(new Date(now.getTime() + TOKEN_TIME))
+            .signWith(key, Jwts.SIG.HS256)
+            .compact();
+    }
+
+    // 토큰 검증
+    public boolean validateToken(String token) {
+
+        if (token == null || token.isBlank()) {
+            return false;
+        }
+
+        try {
+            parser.parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    // 토큰 복호화
+    private Claims extractAllClaims(String token) {
+        return parser.parseSignedClaims(token).getPayload();
+    }
+
+    public Long extractId(String token) {
+        return extractAllClaims(token).get("id", Long.class);
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).get("email", String.class);
+    }
+
+    public String extractNickname(String token) {
+        return extractAllClaims(token).get("nickname", String.class);
+    }
+}
