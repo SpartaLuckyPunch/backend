2e19e1472376881eebc10b3e4bbe9b93943d830e
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-01-16 20:55:51 +0900

    refactor: 사용하지 않는 import 삭제

--- src/main/java/com/example/burnchuck/domain/user/service/UserService.java
+++ src/main/java/com/example/burnchuck/domain/user/service/UserService.java
@@ -15,7 +15,6 @@ import com.example.burnchuck.domain.user.repository.AddressRepository;
 import com.example.burnchuck.domain.user.repository.UserRepository;
 import java.util.List;
 import java.util.Objects;
-import java.util.OptionalDouble;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
