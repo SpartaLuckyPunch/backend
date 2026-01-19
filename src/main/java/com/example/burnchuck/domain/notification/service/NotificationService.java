ada6cd35ad40a5f73f82bf7fb0554bef704a3c1d
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-01-19 17:37:50 +0900

    feat: '팔로우 유저의 모임 생성'에 대한 알림 생성 메서드 구현
    1. 모임을 생성한 유저의 팔로워 목록 조회
    2. 생성한 meeting 내용에 맞게 알림 설명글 수정
    3. 팔로워 리스트 순회하며 대상 유저를 넣어 알림 객체 생성
      (고도화 작업에서 필요시 insert 방법 변경)
    4. 알림 저장

--- src/main/java/com/example/burnchuck/domain/notification/service/NotificationService.java
+++ src/main/java/com/example/burnchuck/domain/notification/service/NotificationService.java
@@ -1,11 +1,16 @@
 package com.example.burnchuck.domain.notification.service;
 
+import com.example.burnchuck.common.entity.Follow;
+import com.example.burnchuck.common.entity.Meeting;
 import com.example.burnchuck.common.entity.Notification;
+import com.example.burnchuck.common.entity.User;
+import com.example.burnchuck.common.enums.NotificationType;
+import com.example.burnchuck.domain.follow.repository.FollowRepository;
 import com.example.burnchuck.domain.notification.repository.NotificationRepository;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -13,13 +18,40 @@ import org.springframework.transaction.annotation.Transactional;
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final FollowRepository followRepository;
 
     /**
-     * 알림 생성 및 저장
+     * 유저가 모임을 생성했을 때 -> 해당 유저를 팔로우하는 사람에게 알림 발송
      */
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void createNotification(List<Notification> notification) {
+    @Transactional
+    public void notifNewFollowerPost(Meeting meeting, User user) {
 
-        notificationRepository.saveAll(notification);
+        NotificationType notificationType = NotificationType.NEW_FOLLOWING_POST;
+
+        // 1. 모임을 생성한 유저의 팔로워 목록 조회
+        List<Follow> followerList = followRepository.findAllByFollowee(user);
+
+        // 2. 생성한 meeting 내용에 맞게 알림 설명글 수정
+        String description = notificationType.getDescription();
+        description = description.replace("{nickname}", user.getNickname());
+        description = description.replace("{title}", meeting.getTitle());
+
+        // 3. 팔로워 리스트 순회하며 대상 유저를 넣어 알림 객체 생성
+        List<Notification> notificationList = new ArrayList<>();
+
+        for (Follow follow : followerList) {
+
+            Notification notification = new Notification(
+                notificationType,
+                description,
+                follow.getFollower(),
+                meeting
+            );
+
+            notificationList.add(notification);
+        }
+
+        // 4. 알림 저장
+        notificationRepository.saveAll(notificationList);
     }
 }
