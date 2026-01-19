e6c63ec38c1653d048c22bf8f2a27fcdefc067e2
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-01-19 17:35:15 +0900

    refactor: 띄어쓰기 삭제

--- src/main/java/com/example/burnchuck/common/enums/NotificationType.java
+++ src/main/java/com/example/burnchuck/common/enums/NotificationType.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum NotificationType {
 
-    NEW_FOLLOWING_POST("\'{nickname}\'님이 새 모임 \'{title}\' 을/를 생성하였습니다."),
+    NEW_FOLLOWING_POST("\'{nickname}\'님이 새 모임 \'{title}\'을/를 생성하였습니다."),
     MEETING_MEMBER_JOIN("\'{title}\' 모임에 \'{nickname}\'님이 참여하셨습니다."),
     MEETING_MEMBER_LEFT("\'{title}\' 모임에 \'{nickname}\'님이 탈퇴하셨습니다."),
     COMMENT_REQUESTED("\'{title}\' 모임, 어떠셨나요?\n만났던 사람들의 후기를 남겨주세요!");
