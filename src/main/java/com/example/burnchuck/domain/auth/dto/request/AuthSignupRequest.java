5d8e6bfb9ab5e1a95d560637dbe7fcb95a7f7f0b
Thugi-98 <kimjh4533@gmail.com>
2026-01-22 19:27:43 +0900

    Update AuthSignupRequest.java
    refactor: 공백 수정

--- src/main/java/com/example/burnchuck/domain/auth/dto/request/AuthSignupRequest.java
+++ src/main/java/com/example/burnchuck/domain/auth/dto/request/AuthSignupRequest.java
@@ -6,12 +6,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-
 import java.time.LocalDate;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
 
 @Getter
 @AllArgsConstructor
