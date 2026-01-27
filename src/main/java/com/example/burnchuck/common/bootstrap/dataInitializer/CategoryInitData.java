16d969f7ca594c2a446822ace0e295a4fbb12bc9
Thugi-98 <kimjh4533@gmail.com>
2026-01-27 09:27:46 +0900

    refactor: InitData 구조 통일화, Swagger API 순서 재배치

--- src/main/java/com/example/burnchuck/common/bootstrap/dataInitializer/CategoryInitData.java
+++ src/main/java/com/example/burnchuck/common/bootstrap/dataInitializer/CategoryInitData.java
@@ -1,9 +1,7 @@
 package com.example.burnchuck.common.bootstrap.dataInitializer;
 
 import com.example.burnchuck.common.entity.Category;
-import com.example.burnchuck.common.entity.Reaction;
 import com.example.burnchuck.domain.category.repository.CategoryRepository;
-import com.example.burnchuck.domain.reaction.repository.ReactionRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
@@ -12,15 +10,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class CategoryReactionInitData implements ApplicationRunner {
+public class CategoryInitData implements ApplicationRunner {
 
     private final CategoryRepository categoryRepository;
-    private final ReactionRepository reactionRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        if (categoryRepository.count() > 0 || reactionRepository.count() > 0 ) {
+        if (categoryRepository.count() > 0) {
             return;
         }
 
@@ -36,19 +33,5 @@ public class CategoryReactionInitData implements ApplicationRunner {
         );
 
         categoryRepository.saveAll(categoryList);
-
-        List<Reaction> reactionList = List.of(
-            new Reaction("친절하고 매너가 좋아요."),
-            new Reaction("시간 약속을 잘 지켜요."),
-            new Reaction("분위기를 잘 살려요."),
-            new Reaction("유머 감각이 있어요."),
-            new Reaction("이야기를 잘 들어줘요."),
-            new Reaction("에너지가 좋아요."),
-            new Reaction("적극적으로 참여해요."),
-            new Reaction("아이디어를 잘 내요."),
-            new Reaction("배려심이 있어요.")
-        );
-
-        reactionRepository.saveAll(reactionList);
     }
 }
