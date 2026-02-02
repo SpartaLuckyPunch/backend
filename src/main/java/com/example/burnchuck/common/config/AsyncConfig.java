0f2bc6dd22e8be1c00b4ad840ce42ffe5b7e0d31
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-02-02 17:36:31 +0900

    feat: 비동기 상황 예외 처리 로직 추가
    - 비동기 처리 상황에서 예외 발생 시, 예외 발생 메서드, 예외 메시지 로깅

--- src/main/java/com/example/burnchuck/common/config/AsyncConfig.java
+++ src/main/java/com/example/burnchuck/common/config/AsyncConfig.java
@@ -1,15 +1,18 @@
 package com.example.burnchuck.common.config;
 
+import com.example.burnchuck.common.exception.AsyncExceptionHandler;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
 
     private static final int CORE_POOL_SIZE = 4;
     private static final int MAX_POOL_SIZE = 8;
@@ -25,5 +28,10 @@ public class AsyncConfig {
         executor.initialize();
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncExceptionHandler();
     }
 }
