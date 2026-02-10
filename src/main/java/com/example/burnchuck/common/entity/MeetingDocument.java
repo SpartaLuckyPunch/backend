917150935a7f823910b25cb8b9db804c151c4d7b
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-02-10 22:46:29 +0900

    feat: 출력 내용에 따라 필드 추가

--- src/main/java/com/example/burnchuck/common/entity/MeetingDocument.java
+++ src/main/java/com/example/burnchuck/common/entity/MeetingDocument.java
@@ -1,10 +1,12 @@
 package com.example.burnchuck.common.entity;
 
-import java.time.LocalDate;
+import com.example.burnchuck.common.enums.MeetingStatus;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -26,18 +28,38 @@ public class MeetingDocument {
 
     private GeoPoint geoPoint;
 
-    @Field(type = FieldType.Date)
-    private LocalDate meetingDate;
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime meetingDatetime;
 
     @Field(type = FieldType.Integer)
-    private Integer meetingTime;
+    private Integer meetingHour;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime createdDatetime;
+
+    @Field(type = FieldType.Keyword)
+    private MeetingStatus status;
+
+    @Field(type = FieldType.Keyword)
+    private String imgUrl;
+
+    @Field(type = FieldType.Keyword)
+    private String location;
+
+    @Field(type = FieldType.Integer)
+    private int maxAttendees;
 
     public MeetingDocument(Meeting meeting) {
         this.id = String.valueOf(meeting.getId());
         this.title = meeting.getTitle();
         this.categoryCode = meeting.getCategory().getCode();
         this.geoPoint = new GeoPoint(meeting.getLatitude(), meeting.getLongitude());
-        this.meetingDate = meeting.getMeetingDateTime().toLocalDate();
-        this.meetingTime = meeting.getMeetingDateTime().getHour();
+        this.meetingDatetime = meeting.getMeetingDateTime();
+        this.meetingHour = meeting.getMeetingDateTime().getHour();
+        this.createdDatetime = meeting.getCreatedDatetime();
+        this.status = meeting.getStatus();
+        this.imgUrl = meeting.getImgUrl();
+        this.location = meeting.getLocation();
+        this.maxAttendees = meeting.getMaxAttendees();
     }
 }
