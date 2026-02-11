1cc2e90dc914294bf4b3d8fc6732a2c441eebbea
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-02-11 20:55:40 +0900

    refactor: import 순서 변경

--- src/main/java/com/example/burnchuck/common/dto/PageResponse.java
+++ src/main/java/com/example/burnchuck/common/dto/PageResponse.java
@@ -1,9 +1,10 @@
 package com.example.burnchuck.common.dto;
 
-import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
