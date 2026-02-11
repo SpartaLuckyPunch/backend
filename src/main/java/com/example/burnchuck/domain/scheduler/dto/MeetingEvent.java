c4d1002cce17d9dc9bd864a87f8c20f6122d6d97
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-02-11 15:19:16 +0900

    refactor: Event 객체 내 Type으로 구분

--- src/main/java/com/example/burnchuck/domain/scheduler/dto/MeetingEvent.java
+++ src/main/java/com/example/burnchuck/domain/scheduler/dto/MeetingEvent.java
@@ -1,12 +1,14 @@
 package com.example.burnchuck.domain.scheduler.dto;
 
 import com.example.burnchuck.common.entity.Meeting;
+import com.example.burnchuck.common.enums.MeetingTaskType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class MeetingDeletedEvent {
+public class MeetingEvent {
 
+    private MeetingTaskType type;
     private Meeting meeting;
 }
--- src/main/java/com/example/burnchuck/domain/scheduler/service/EventPublisherService.java
+++ src/main/java/com/example/burnchuck/domain/scheduler/service/EventPublisherService.java
@@ -1,9 +1,8 @@
 package com.example.burnchuck.domain.scheduler.service;
 
 import com.example.burnchuck.common.entity.Meeting;
-import com.example.burnchuck.domain.scheduler.dto.MeetingCreatedEvent;
-import com.example.burnchuck.domain.scheduler.dto.MeetingDeletedEvent;
-import com.example.burnchuck.domain.scheduler.dto.MeetingUpdatedEvent;
+import com.example.burnchuck.common.enums.MeetingTaskType;
+import com.example.burnchuck.domain.scheduler.dto.MeetingEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -16,19 +15,19 @@ public class EventPublisherService {
 
     public void publishMeetingCreatedEvent(Meeting meeting) {
 
-        MeetingCreatedEvent event = new MeetingCreatedEvent(meeting);
+        MeetingEvent event = new MeetingEvent(MeetingTaskType.CREATE, meeting);
         publisher.publishEvent(event);
     }
 
     public void publishMeetingUpdatedEvent(Meeting meeting) {
 
-        MeetingUpdatedEvent event = new MeetingUpdatedEvent(meeting);
+        MeetingEvent event = new MeetingEvent(MeetingTaskType.UPDATE, meeting);
         publisher.publishEvent(event);
     }
 
     public void publishMeetingDeletedEvent(Meeting meeting) {
 
-        MeetingDeletedEvent event = new MeetingDeletedEvent(meeting);
+        MeetingEvent event = new MeetingEvent(MeetingTaskType.DELETE, meeting);
         publisher.publishEvent(event);
     }
 }
