6e3acb7b530730050b637fb35bdd9abaacc13312
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-02-11 11:24:12 +0900

    refactor: 기존 조회 로직 삭제

--- src/main/java/com/example/burnchuck/domain/meeting/repository/MeetingCustomRepository.java
+++ src/main/java/com/example/burnchuck/domain/meeting/repository/MeetingCustomRepository.java
@@ -1,10 +1,7 @@
 package com.example.burnchuck.domain.meeting.repository;
 
 import com.example.burnchuck.common.entity.Meeting;
-import com.example.burnchuck.domain.meeting.dto.request.MeetingSearchRequest;
 import com.example.burnchuck.domain.meeting.dto.response.MeetingDetailResponse;
-import com.example.burnchuck.domain.meeting.dto.response.MeetingMapPointResponse;
-import com.example.burnchuck.domain.meeting.dto.response.MeetingSummaryResponse;
 import com.example.burnchuck.domain.meeting.dto.response.MeetingSummaryWithStatusResponse;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,8 +11,6 @@ import org.springframework.data.domain.Pageable;
 
 public interface MeetingCustomRepository {
 
-    Page<MeetingSummaryResponse> findMeetingList(MeetingSearchRequest searchRequest, List<Long> meetingIdList, Pageable pageable);
-
     Optional<MeetingDetailResponse> findMeetingDetail(Long meetingId);
 
     Page<MeetingSummaryWithStatusResponse> findHostedMeetings(Long userId, Pageable pageable);
@@ -23,6 +18,4 @@ public interface MeetingCustomRepository {
     List<Meeting> findActiveHostedMeetings(Long userId);
 
     List<Meeting> findActivateMeetingsForNotification(LocalDateTime startDate, LocalDateTime endDate);
-
-    List<MeetingMapPointResponse> findMeetingPointList(List<Long> meetingIdList);
 }
--- src/main/java/com/example/burnchuck/domain/meeting/repository/MeetingCustomRepositoryImpl.java
+++ src/main/java/com/example/burnchuck/domain/meeting/repository/MeetingCustomRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.example.burnchuck.domain.meeting.repository;
 
-import static com.example.burnchuck.common.entity.QCategory.category1;
 import static com.example.burnchuck.common.entity.QMeeting.meeting;
 import static com.example.burnchuck.common.entity.QMeetingLike.meetingLike;
 import static com.example.burnchuck.common.entity.QNotification.notification;
@@ -8,122 +7,24 @@ import static com.example.burnchuck.common.entity.QUserMeeting.userMeeting;
 
 import com.example.burnchuck.common.entity.Meeting;
 import com.example.burnchuck.common.enums.MeetingRole;
-import com.example.burnchuck.common.enums.MeetingSortOption;
 import com.example.burnchuck.common.enums.MeetingStatus;
 import com.example.burnchuck.common.enums.NotificationType;
-import com.example.burnchuck.domain.meeting.dto.request.MeetingSearchRequest;
 import com.example.burnchuck.domain.meeting.dto.response.MeetingDetailResponse;
-import com.example.burnchuck.domain.meeting.dto.response.MeetingMapPointResponse;
-import com.example.burnchuck.domain.meeting.dto.response.MeetingSummaryResponse;
 import com.example.burnchuck.domain.meeting.dto.response.MeetingSummaryWithStatusResponse;
-import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.NumberTemplate;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
 
 @RequiredArgsConstructor
 public class MeetingCustomRepositoryImpl implements MeetingCustomRepository {
 
     private final JPAQueryFactory queryFactory;
-
-    /**
-     * 모임 전체 조회
-     */
-    @Override
-    public Page<MeetingSummaryResponse> findMeetingList(
-            MeetingSearchRequest request,
-            List<Long> meetingIdList,
-            Pageable pageable
-    ) {
-        MeetingSortOption sort = request.getOrder() == null ? MeetingSortOption.LATEST : request.getOrder();
-
-        // TODO : 이렇게 하면 일정 범위 검색할 때 어떤 정렬을 사용하고 싶어도 사용할 수 없음
-        if (request.getStartDate() != null || request.getEndDate() != null) {
-            sort = MeetingSortOption.UPCOMING;
-        }
-
-        OrderSpecifier<?> orderSpecifier =
-            switch (sort) {
-                case POPULAR -> meetingLike.id.countDistinct().desc();
-                case NEAREST -> meeting.id.asc();
-                case UPCOMING -> meeting.meetingDateTime.asc();
-                case LATEST -> meeting.createdDatetime.desc();
-            };
-
-        List<MeetingSummaryResponse> content = queryFactory
-            .select(Projections.constructor(
-                    MeetingSummaryResponse.class,
-                    meeting.id,
-                    meeting.title,
-                    meeting.imgUrl,
-                    meeting.location,
-                    meeting.latitude,
-                    meeting.longitude,
-                    meeting.meetingDateTime,
-                    meeting.maxAttendees,
-                    userMeeting.id.countDistinct().intValue()
-            ))
-            .from(meeting)
-            .leftJoin(userMeeting)
-            .on(userMeeting.meeting.eq(meeting))
-            .leftJoin(meeting.category, category1)
-            .where(
-                    meeting.status.eq(MeetingStatus.OPEN),
-                    inMeetingIdList(meetingIdList)
-            )
-            .groupBy(meeting.id)
-            .orderBy(orderSpecifier)
-            .offset(pageable.getOffset())
-            .limit(pageable.getPageSize())
-            .fetch();
-
-        JPAQuery<Long> countQuery = queryFactory
-            .select(meeting.count())
-            .from(meeting)
-            .leftJoin(meeting.category, category1)
-            .where(
-                meeting.status.eq(MeetingStatus.OPEN),
-                inMeetingIdList(meetingIdList)
-            );
-
-        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
-    }
-
-    /**
-     * 모임 지도 조회
-     */
-    @Override
-    public List<MeetingMapPointResponse> findMeetingPointList(
-        List<Long> meetingIdList
-    ) {
-        return queryFactory
-            .select(Projections.constructor(
-                MeetingMapPointResponse.class,
-                meeting.id,
-                meeting.title,
-                meeting.latitude,
-                meeting.longitude
-            ))
-            .from(meeting)
-            .leftJoin(meeting.category, category1)
-            .where(
-                meeting.status.eq(MeetingStatus.OPEN),
-                inMeetingIdList(meetingIdList)
-            )
-            .fetch();
-    }
 
     /**
      * 모임 상세 조회
@@ -239,36 +140,5 @@ public class MeetingCustomRepositoryImpl implements MeetingCustomRepository {
                 meeting.meetingDateTime.between(startDate, endDate)
             )
             .fetch();
-    }
-
-    private BooleanExpression inMeetingIdList(List<Long> meetingIdList) {
-
-        if (meetingIdList == null) {
-            return null;
-        }
-
-        return meeting.id.in(meetingIdList);
-    }
-
-    /**
-     * 주어진 리스트 순서대로 정렬
-     */
-    private OrderSpecifier<Long> orderByListOrder(List<Long> meetingIdList) {
-
-        if (meetingIdList == null || meetingIdList.isEmpty()) {
-            return meeting.id.asc();
-        }
-
-        String ids = meetingIdList.stream()
-            .map(String::valueOf)
-            .collect(Collectors.joining(","));
-
-        NumberTemplate<Long> orderExpr = Expressions.numberTemplate(
-            Long.class,
-            "FIELD({0}, " + ids + ")",
-            meeting.id
-        );
-
-        return orderExpr.asc();
     }
 }
