a33690a59846ab2f9b2dfb54bf2ac3e9c89fed97
sangmu <dltkdandl@naver.com>
2026-02-12 22:20:53 +0900

    refactor: 2.7 WebSocket 엔드포인트에 인증 없음 + 와일드카드 Origin 허용 수정

---
+++ src/main/java/com/example/burnchuck/common/handler/AuthHandshakeInterceptor.java
@@ -0,0 +1,60 @@
+package com.example.burnchuck.common.handler;
+
+import com.example.burnchuck.common.utils.JwtUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.lang.NonNull;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthHandshakeInterceptor implements HandshakeInterceptor {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public boolean beforeHandshake(@NonNull ServerHttpRequest request, @NonNull ServerHttpResponse response,
+                                   @NonNull WebSocketHandler wsHandler, @NonNull Map<String, Object> attributes) {
+
+        if (request instanceof ServletServerHttpRequest servletRequest) {
+            HttpServletRequest req = servletRequest.getServletRequest();
+
+            String token = null;
+            Cookie[] cookies = req.getCookies();
+
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if ("accessToken".equals(cookie.getName())) {
+                        token = cookie.getValue();
+                        break;
+                    }
+                }
+            }
+
+            if (token != null && jwtUtil.validateToken(token)) {
+                attributes.put("accessToken", token);
+                log.info("Handshake 성공: 유효한 토큰 발견");
+                return true;
+            } else {
+                log.warn("Handshake 실패: 토큰이 없거나 유효하지 않음");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void afterHandshake(@NonNull ServerHttpRequest request, @NonNull ServerHttpResponse response,
+                               @NonNull WebSocketHandler wsHandler, Exception exception) {
+    }
+}
