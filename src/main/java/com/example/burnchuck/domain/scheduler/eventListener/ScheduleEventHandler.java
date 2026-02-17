f37d59e3be3efd4b3d961e364af79ab9929bdaad
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-02-17 12:15:53 +0900

    refactor: 이벤트 중지

--- src/main/java/com/example/burnchuck/domain/scheduler/eventListener/ScheduleEventHandler.java
+++ src/main/java/com/example/burnchuck/domain/scheduler/eventListener/ScheduleEventHandler.java
@@ -2,15 +2,11 @@ package com.example.burnchuck.domain.scheduler.eventListener;
 
 import com.example.burnchuck.common.entity.Meeting;
 import com.example.burnchuck.common.enums.MeetingTaskType;
-import com.example.burnchuck.domain.meeting.repository.MeetingRepository;
 import com.example.burnchuck.domain.meeting.event.MeetingEvent;
+import com.example.burnchuck.domain.meeting.repository.MeetingRepository;
 import com.example.burnchuck.domain.scheduler.service.SchedulingService;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -28,24 +24,24 @@ public class ScheduleEventHandler {
     /**
      * 어플리케이션 재시작 후, 삭제된 이벤트 복구
      */
-    @EventListener(ApplicationReadyEvent.class)
-    public void restoreSchedules() {
-
-        List<Meeting> meetingList = meetingRepository.findActivateMeetingsForSchedules();
-
-        meetingList.forEach(meeting -> {
-            schedulingService.scheduleMeetingStatusComplete(meeting);
-            schedulingService.scheduleNotification(meeting);
-        });
-
-        LocalDate today = LocalDate.now();
-        LocalDateTime startDate = today.minusDays(1).atStartOfDay();
-        LocalDateTime endDate = today.plusDays(1).atStartOfDay();
-
-        List<Meeting> requireNotificationList = meetingRepository.findActivateMeetingsForNotification(startDate, endDate);
-
-        requireNotificationList.forEach(schedulingService::scheduleNotification);
-    }
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void restoreSchedules() {
+//
+//        List<Meeting> meetingList = meetingRepository.findActivateMeetingsForSchedules();
+//
+//        meetingList.forEach(meeting -> {
+//            schedulingService.scheduleMeetingStatusComplete(meeting);
+//            schedulingService.scheduleNotification(meeting);
+//        });
+//
+//        LocalDate today = LocalDate.now();
+//        LocalDateTime startDate = today.minusDays(1).atStartOfDay();
+//        LocalDateTime endDate = today.plusDays(1).atStartOfDay();
+//
+//        List<Meeting> requireNotificationList = meetingRepository.findActivateMeetingsForNotification(startDate, endDate);
+//
+//        requireNotificationList.forEach(schedulingService::scheduleNotification);
+//    }
 
     /**
      * MeetingCreatedEvent에 대한 Handler -> TaskSchedule 생성
