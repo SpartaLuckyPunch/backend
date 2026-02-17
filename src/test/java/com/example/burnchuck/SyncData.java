1106a0dca6985d6888e86a685b67b76e842c2073
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-02-17 12:10:21 +0900

    test: Redis, ES 동기화용 테스트 코드 추가

---
+++ src/test/java/com/example/burnchuck/SyncData.java
@@ -0,0 +1,72 @@
+package com.example.burnchuck;
+
+import com.example.burnchuck.common.document.MeetingDocument;
+import com.example.burnchuck.common.entity.Meeting;
+import com.example.burnchuck.domain.meeting.repository.MeetingRepository;
+import com.example.burnchuck.domain.meeting.service.MeetingCacheService;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+
+@SpringBootTest
+public class SyncData {
+
+    @Autowired
+    private MeetingRepository meetingRepository;
+    @Autowired
+    private MeetingCacheService meetingCacheService;
+    @Autowired
+    private ElasticsearchOperations elasticsearchOperations;
+
+    private static final int BATCH_SIZE = 10000;
+
+    @Test
+    @DisplayName("ES 동기화")
+    void syncES() {
+
+        long lastId = 0L;
+
+        while (true) {
+
+            List<Meeting> meetingList = meetingRepository.findMeetingListForSync(lastId, PageRequest.of(0, BATCH_SIZE));
+
+            if (meetingList.isEmpty()) {
+                break;
+            }
+
+            List<MeetingDocument> documentList = meetingList.stream()
+                .map(meeting -> new MeetingDocument(meeting, 0, 0))
+                .toList();
+
+            elasticsearchOperations.save(documentList);
+
+            lastId = meetingList.get(meetingList.size() -1).getId();
+        }
+    }
+
+    @Test
+    @DisplayName("Redis GEO 동기화")
+    void syncRedis() {
+
+        long lastId = 0L;
+
+        while (true) {
+
+            List<Meeting> meetingList = meetingRepository.findMeetingListForSync(lastId, PageRequest.of(0, BATCH_SIZE));
+
+            if (meetingList.isEmpty()) {
+                break;
+            }
+
+            for (Meeting meeting : meetingList) {
+//                meetingCacheService.saveMeetingLocation(meeting);
+            }
+
+            lastId = meetingList.get(meetingList.size() -1).getId();
+        }
+    }
+}
